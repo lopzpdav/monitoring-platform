3da2fe84fb155208eefb5667762ea1eebdea8042
Patricio López <lopzpdav@gmail.com>
2024-10-01 18:16:59 -0500

    firs commit

---
+++ src/main/java/org/tesis/monitoringplatform/MonitoringPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.tesis.monitoringplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringPlatformApplication.class, args);
+	}
+
+}
